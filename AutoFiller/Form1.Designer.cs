347bbd5548a288325ecb14f38b5f34fa46463d45
Jack <Jack@JACK-DESKTOP>
2017-07-06 18:30:35 -0400

    Add project files.

---
+++ AutoFiller/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace AutoFiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(555, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "File Location";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(555, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Location To Be Filled";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(555, 37);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Fill";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 147);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(555, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 191);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Filler";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ AutoFiller/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoFiller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string sourceFolder = "";
+        string[] sourceFiles = new string[] { };
+        string targetFolder = "";
+        string[] targetFiles = new string[] { };
+        List<string> sfnames = new List<string> { };
+        List<string> tfnames = new List<string> { };
+
+        //Create txt file on desktop and update fileLoc below with its path to save already existing files that aren't copied
+        public string fileLoc = @"C:\Users\Jack\Desktop\ExistingFiles.txt";
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                sourceFolder = dlg.SelectedPath;
+
+            }
+
+            sourceFiles = Directory.GetFiles(sourceFolder, "*.*", SearchOption.AllDirectories);
+            foreach (string f in sourceFiles)
+            {
+                string sfn;
+                sfn = Path.GetFileName(f);
+                sfnames.Add(sfn);
+            }
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fillHere = new FolderBrowserDialog();
+
+            if (fillHere.ShowDialog() == DialogResult.OK)
+            {
+                targetFolder = fillHere.SelectedPath;
+            }
+            targetFiles = Directory.GetFiles(targetFolder, "*.*", SearchOption.AllDirectories);
+            foreach (string t in targetFiles)
+            {
+                string tfn;
+                tfn = Path.GetFileName(t);
+                tfnames.Add(tfn);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //File Location to write already existed files
+            //Update FileLoc to your desired output location
+            
+            FileStream fs = null;
+            if (!File.Exists(fileLoc))
+            {
+                using (fs = File.Create(fileLoc)) { };
+            }
+
+            DirectoryCopy(sourceFolder, targetFolder, true);
+            MessageBox.Show("Copying Complete");
+        }
+
+        private void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            // If the destination directory doesn't exist, create it. 
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destDirName, file.Name);
+                if (!File.Exists(tempPath))
+                {
+                    file.CopyTo(tempPath, false);
+                }
+                else
+                {
+                    using (StreamWriter sw = new StreamWriter(fileLoc, true))
+                    {
+                        sw.WriteLine(tempPath);
+                    }
+                }
+            }
+
+            // If copying subdirectories, copy them and their contents to new location. 
+            if (copySubDirs)
+             {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
+        }
+    }
+}
+        
